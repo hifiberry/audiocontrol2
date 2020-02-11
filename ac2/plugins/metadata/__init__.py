9bb7b886fef06ec397a58339bb605e2b9756c696
HiFiBerry <info@hifiberry.com>
2020-02-11 12:19:25 +0100

    Allow a notifier to wait a bit on an update thread that's running

--- ac2/plugins/metadata/__init__.py
+++ ac2/plugins/metadata/__init__.py
@@ -23,6 +23,7 @@ SOFTWARE.
 import threading
 import logging
 import datetime
+import time
 
 
 class MetadataDisplay:
@@ -31,20 +32,26 @@ class MetadataDisplay:
         logging.debug("initializing MetadataDisplay instance")
         self.notifierthread = None
         self.starttime = None
+        self.async_delay = 1
         pass
 
     def notify(self, metadata):
         raise RuntimeError("notify not implemented")
 
     def notify_async(self, metadata):
-        if self.notifierthread is None or not(self.notifierthread.is_alive()):
+        # Don't run 2 async notifier threads in parallel
+        # If there is already one running. wait a bit and try again
+        if self.notifierthread is not None and self.notifierthread.is_alive():
+            time.sleep(self.async_delay)
+        
+        if self.notifierthread is not None and self.notifierthread.is_alive():            
+            logging.info("notifier background thread %s still running after %s seconds, "
+                         "not sending notify",
+                         self.notifierthread,
+                         datetime.datetime.now() - self.notifystarttime)
+        else:
             self.notifierthread = threading.Thread(target=self.notify,
-                                                   args=(metadata,),
-                                                   name="notifier thread "+self.__str__())
+                                                       args=(metadata,),
+                                                       name="notifier thread "+self.__str__())
             self.notifystarttime = datetime.datetime.now()
             self.notifierthread.start()
-        else:
-            logging.info("notifier background thread %s still running after %s seconds, "
-                         "not sending notify", 
-                         datetime.datetime.now() - self.notifystarttime,
-                         self.notifierthread)
